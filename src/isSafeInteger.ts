9172fd5f58b99798dd6ec2726d1f93abfb93dee4
liuyun <liuyunjs@liuyundeMacBook-Pro.local>
2021-05-16 16:58:00 +0800

    update safe integer

--- src/isSafeInteger.ts
+++ src/isSafeInteger.ts
@@ -1,9 +1,10 @@
 import { isInteger } from './isInteger';
 
-const MAX_SAFE_NUMBER = Math.pow(2, 53) - 1;
+const MAX_SAFE_NUMBER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1;
+const MIN_SAFE_NUMBER = Number.MIN_SAFE_INTEGER || -MAX_SAFE_NUMBER;
 
 export const isSafeInteger = (Number.isSafeInteger ||
   ((val: any): val is number =>
-    isInteger(val) && val <= MAX_SAFE_NUMBER && val >= -MAX_SAFE_NUMBER)) as (
+    isInteger(val) && val <= MAX_SAFE_NUMBER && val >= MIN_SAFE_NUMBER)) as (
   val: any,
 ) => val is number;
