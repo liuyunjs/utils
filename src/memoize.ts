c09ed61e1ef4e121da75352535d16fc86af2a9a6
liuyunjs <429695034@qq.com>
2021-05-24 14:34:40 +0800

    <fix>: memoize 第二个参数可选

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,11 +1,8 @@
-import { isPrimitive } from './isPrimitive';
-import { isUndefined } from './isUndefined';
-import { isSymbol } from './isSymbol';
 import { Once } from './once';
 
 export function memoize<T extends (...args: any[]) => any>(
   callback: T,
-  resolver: (...args: Parameters<T>) => any,
+  resolver?: (...args: Parameters<T>) => any,
 ) {
   if (process.env.NODE_ENV !== 'production') {
     if (typeof callback !== 'function') {
@@ -16,29 +13,23 @@ export function memoize<T extends (...args: any[]) => any>(
     }
   }
 
-  let cachedObj: Record<string | number, { value: ReturnType<T> }> = {};
-  let cachedMap = new (WeakMap || Map)<any, { value: ReturnType<T> }>();
+  const cached = new Map<any, { value: ReturnType<T> }>();
 
   function wrapped() {
     let args = arguments,
       key = resolver ? resolver.apply(this, args) : args[0];
-    if (isPrimitive(key)) {
-      if (!isSymbol(key)) key = key + '';
-      if (!(key in cachedObj)) cachedObj[key] = { value: callback.apply(this, args) };
-      return cachedObj[key].value;
-    }
-    let result = cachedMap.get(key);
 
-    if (isUndefined(result)) {
+    let result = cached.get(key);
+
+    if (!result) {
       result = { value: callback.apply(this, args) };
-      cachedMap.set(key, result);
+      cached.set(key, result);
     }
     return result.value;
   }
 
   wrapped.clear = function () {
-    cachedObj = {};
-    cachedMap = new (WeakMap || Map)<any, { value: ReturnType<T> }>();
+    cached.clear();
   };
 
   return wrapped as Once<T>;
