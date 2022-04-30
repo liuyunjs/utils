f25ce2a918695224d014a825a8301e357591e2a6
liuyunjs <429695034@qq.com>
2022-04-30 23:26:29 +0800

    1.2.2 get 导出异常

--- src/get.ts
+++ src/get.ts
@@ -1,5 +1,7 @@
+import justGet from 'just-safe-get';
+
 export const get: (
   item: any[] | {},
   target: string | string[],
   defaultValue?: any,
-) => any = require('just-safe-get');
+) => any = justGet;
--- src/uniqueId.ts
+++ src/uniqueId.ts
@@ -1,5 +1,5 @@
 export const uniqueId = (prefix?: string) => {
-  const str = Math.random().toString(32);
+  const str = Math.random().toString(32).slice(2);
   if (!prefix) return str;
   return `${prefix}.${str}`;
 };
